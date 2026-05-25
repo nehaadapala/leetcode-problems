1e6047c29fb8ad1f2984a03930a7682f2d8832a5
nehaadapala <nehasriiadapala@gmail.com>
2026-05-25 14:06:30 +0530

    Add files via upload

---
+++ Palindrome-LinkedList.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+
+   
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+      
+        if (fast != null) { 
+            slow = slow.next;
+        }
+
+      
+        ListNode second = reverse(slow);
+        ListNode first = head;
+
+        while (second != null) {
+            if (first.val != second.val) {
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+        }
+
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode nextTemp = head.next;
+            head.next = prev;
+            prev = head;
+            head = nextTemp;
+        }
+        return prev;
+    }
+}
